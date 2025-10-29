e3361eaeed702d82a262d9ff246ec83ed991ca8a
Jesús Enrique Rascón <blograso@gmail.com>
2025-10-29 18:06:44 +0100

    auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@deno/svelte-adapter';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		// experimental: {
-		// 	remoteFunctions: true
-		// },
+		experimental: {
+			remoteFunctions: true
+		},
 	},
 	compilerOptions: {
 		experimental: {
